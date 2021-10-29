a01e6e8653ae228915ae751708f9b79743d8d21d
takahashitakuya <takahashitakuya@192.168.3.78>
2021-10-29 15:44:19 +0900

    tasklistのIndex/NewServletの修正

--- src/controllers/IndexServlet.java
+++ src/controllers/IndexServlet.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.List;
 
 import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -13,9 +14,7 @@ import javax.servlet.http.HttpServletResponse;
 import models.Message;
 import utils.DBUtil;
 
-/**
- * Servlet implementation class IndexServlet
- */
+
 @WebServlet("/index")
 public class IndexServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
@@ -31,10 +30,18 @@ public class IndexServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         EntityManager em = DBUtil.createEntityManager();
 
+        //データベースからデータを取得
         List<Message> messages = em.createNamedQuery("getAllMessages", Message.class).getResultList();
         response.getWriter().append(Integer.valueOf(messages.size()).toString());
 
         em.close();
+
+
+        //データベースから取得したメッセージ一覧（messages）をリクエストスコープにセットし、index.jsp を呼び出しています
+        request.setAttribute("messages", messages);
+
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/messages/index.jsp");
+        rd.forward(request, response);
     }
 
 }
